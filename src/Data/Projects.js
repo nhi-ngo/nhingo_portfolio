f175c1f5c435edf5141bb8e0a9842fe45c698df7
nhi-ngo <nhingo.y@gmail.com>
2021-03-03 17:05:16 -0500

    update project

--- src/Data/Projects.js
+++ src/Data/Projects.js
@@ -7,18 +7,28 @@ export const projects = [
     tech: 'React, D3, Material-UI',
     code: 'https://github.com/chingu-voyages/v26-geckos-team-02',
     live: 'https://stateofsafety.netlify.app/',
-    img: '/images/crime.png',
+    img: '/images/crime1.png',
     alt: 'screenshot of state of safety',
   },
 
   {
-    title: 'Cakes API',
+    title: 'Quick Notes',
     description:
-      'In the mood to bake? Well, break out the butter and the rolling pins because I have plenty of recipes for you to try! Take a look at the Cakes API for a single random cake recipe or for all the cake recipes your heart desires!',
-    tech: 'React, Node, Express',
-    code: 'https://github.com/nhi-ngo/cakes-api',
-    live: 'https://my-cake-client.herokuapp.com/',
-    img: '/images/cakes.png',
-    alt: 'screenshot api',
+      'Quick-Notes application allows users to capture quick thoughts that they come across in order to revisit later.',
+    tech: 'React, Node, Express, MongoDB',
+    code: 'https://github.com/nhi-ngo/quick-notes-taking',
+    live: 'https://quick-notes-taking.netlify.app/',
+    img: '/images/notes1.png',
+    alt: 'screenshot note',
   },
+  // {
+  //   title: 'Cakes API',
+  //   description:
+  //     'In the mood to bake? Well, break out the butter and the rolling pins because I have plenty of recipes for you to try! Take a look at the Cakes API for a single random cake recipe or for all the cake recipes your heart desires!',
+  //   tech: 'React, Node, Express',
+  //   code: 'https://github.com/nhi-ngo/cakes-api',
+  //   live: 'https://my-cake-client.herokuapp.com/',
+  //   img: '/images/cakes.png',
+  //   alt: 'screenshot api',
+  // },
 ]
